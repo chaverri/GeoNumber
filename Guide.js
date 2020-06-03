098ca0e3f685c269669f1dd76f00a613b106f331
David Chaverri <>
2020-06-03 14:20:17 +0200

    New 0.2.1 build adding a geohash intro

--- Guide.js
+++ Guide.js
@@ -17,19 +17,25 @@ const Guide = () => {
       onSkip={finishOnboarding}
       pages={[
         {
-          backgroundColor: "#fff",
+          backgroundColor: "#f8f9f9",
+          image: <Image source={require("./img/onboarding/low_res/geohash.png")} />,
+          title: "¿Qué es un código geohash?",
+          subtitle: "Divide el mundo en cuadrantes de aproximadamente 4.7m\u00B2 a los cuales se les asigna un código de números y letras faciles de compartir",
+        },
+        {
+          backgroundColor: "#f8f9f9",
           image: <Image source={require("./img/onboarding/low_res/long_press.png")} />,
           title: "Mantén presionado",
           subtitle: "Para obtener el código de tu ubicación actual",
         },
         {
-          backgroundColor: "#fff",
+          backgroundColor: "#f8f9f9",
           image: <Image source={require("./img/onboarding/low_res/marker_drag.png")} />,
           title: "Mantén el marcador presionado",
           subtitle: "Para arrastrarlo y ver el código de esa ubicación",
         },
         {
-          backgroundColor: "#fff",
+          backgroundColor: "#f8f9f9",
           image: <Image source={require("./img/onboarding/low_res/click_code.png")} />,
           title: "Presiona sobre el código actual",
           subtitle: "Para ver más opciones como compartir o ingresar uno nuevo",
